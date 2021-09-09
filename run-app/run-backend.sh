47ed53c26da6c85c979577d63011c8af1d93379b
apla02 <apla02@gmail.com>
2021-09-09 18:42:39 -0500

    setting up front and backend

--- run-app/run-backend.sh
+++ run-app/run-backend.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/env bash
 echo "Hello from backend!"
+
+# export the env variable
+sudo echo "export PORT=4001" >> /home/vagrant/.bashrc
+source  /home/vagrant/.bashrc
+
+# open the app folder
+cd /shared
+
+# run the process on background
+nohup ./vuego-demoapp > output.log 2>&1 &
+
+# check the execution of the right process
+ps -aux
